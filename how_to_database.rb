37330cd5be66cca046371e19bd0cefb9a4227a6a
Alex Griffith <alex.griffith@flatironschool.com>
2017-09-14 16:47:19 -0400

    Update how_to_database.rb

--- how_to_database.rb
+++ how_to_database.rb
@@ -1,29 +1,7 @@
 require 'sqlite3'
 require 'pry'
 
-# module SQLite3
-#   class Database
-#
-#   end
-#
-# end
 
 db = SQLite3::Database.new('chinook.db')
 
-sql = 'SELECT * FROM artists;'
-db.execute(sql)
-[
-  [1, 'alex', 'griffith'],
-  [2, 'neil']
-]
 
-more_sql = <<-SQL
-SELECT * from tracks
-JOIN albums
-ON tracks.AlbumId = albums.AlbumId
-SQL
-
-db.execute(more_sql)
-
-binding.pry
-"bye"
